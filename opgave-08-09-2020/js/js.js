c556d700ba0bae6de298331310b0ecf0ff017a49
Charlotte <c.libak@hotmail.com>
2020-09-08 12:53:23 +0200

    hurtigere

--- opgave-08-09-2020/js/js.js
+++ opgave-08-09-2020/js/js.js
@@ -1,9 +1,25 @@
 // JavaScript Document
 const isPrime = function (arg) {
-    for (var i = 3; i < arg; i += 2 ) {
-        if (arg % i === 0) {
-            return false;
+    let i = 1 
+    let x = 6 * i - 1
+
+    let y = 6 * i + 1
+    
+    
+    while (i < Math.sqrt(arg)){
+        if( arg === 2 || arg === 3){
+            return true;
         }
+        
+        else if(arg % x ===0 || arg % y === 0)
+            return false;
+            {
+                i ++
+                x = 6 * i - 1
+                y  = 6 * i + 1
+            }
     }
     return true;
+
 }
+
