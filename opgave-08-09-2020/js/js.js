dbb537a70fdc9e408f3d8517dee101713355f65d
Charlotte <c.libak@hotmail.com>
2020-09-08 12:17:40 +0200

    Update js.js

--- opgave-08-09-2020/js/js.js
+++ opgave-08-09-2020/js/js.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 const isPrime = function (arg) {
-    for (var i = 3; i < arg; i += 2) {
+    for (var i = 3; i < arg; i += 2 ) {
         if (arg % i === 0) {
             return false;
         }
